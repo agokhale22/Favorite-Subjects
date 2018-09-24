632c3bafbcba74e618796ba8cc3da46a4cdf66f0
aadityagokhale <aadityagokhale@10.0.0.218>
2018-09-24 15:45:29 -0400

    These are my favorite subjects

---
+++ Favorietsubjects.java
@@ -0,0 +1,14 @@
+//By Aaditya Gokhale Period 1
+public class Favorietsubjects 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("My Favorite Subjects:");
+System.out.print("\t History\n");
+	System.out.println("\t Science");
+	System.out.println("\t Math");
+	System.out.println("\t Computer Sciences");	
+	}
+
+}
