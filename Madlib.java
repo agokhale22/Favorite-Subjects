fe8c1abd3b572897ec1788ccc47174e235cead72
agokhale22 <43002655+agokhale22@users.noreply.github.com>
2018-10-09 08:26:41 -0400

    Add files via upload

---
+++ Madlib.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Madlib 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner keyboard = new Scanner(System.in);
+System.out.print("Enter an adjective:  ");
+	String adjective = keyboard.nextLine();
+	System.out.print("Enter a nationality:  ");
+	String nationality = keyboard.nextLine();
+	System.out.print("Enter a person:  ");
+	String person = keyboard.nextLine();
+System.out.print("Enter a noun:  ");
+	String noun = keyboard.nextLine();
+	System.out.print("Enter give me another adjective:  ");
+	String adjective1 = keyboard.nextLine();
+	System.out.print("Enter another noun:  ");
+	String noun1 = keyboard.nextLine();
+	System.out.print("Enter an adjective:  ");
+	String adjective2 = keyboard.nextLine();
+	System.out.print("Enter an adjective:  ");
+	String adjective3 = keyboard.nextLine();
+	System.out.print("Enter a plural noun:  ");
+	String pluralNoun = keyboard.nextLine();
+	System.out.print("Enter a noun:  ");
+	String noun2 = keyboard.nextLine();
+	System.out.print("Enetr a number:  ");
+	String number = keyboard.nextLine();
+	System.out.print("Enter a shape:  ");
+	String shape = keyboard.nextLine();
+	System.out.print("Enter a food:  ");
+	String food = keyboard.nextLine();
+	System.out.print("Enter a food:  ");
+	String food1 = keyboard.nextLine();
+	System.out.print("Enter a number:  ");
+	String number1 = keyboard.nextLine();
+	System.out.print("Pizza was invented by a " + adjective  +  nationality  + "chef named " + person);
+	
+	
+	
+	
+	
+	}
+	
+	
+	
+	
+	
+}
